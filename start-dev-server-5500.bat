c503b8624def3a35899c93410c7bebc653ce46d9
Military HQ Admin <admin@military-hq.local>
2026-01-07 04:41:54 +0300

    Cleanup: Remove duplicate test files and unnecessary setup scripts
    - Removed test HTML files: PERSISTENCE_TEST.html, SIGNUP_LOGIN_TEST.html, TEST_SIGNUP_LOGIN.html
    - Removed test/setup scripts: test-server.js, test-api.bat, setup.bat, start-dev-server-5500.bat
    - Workspace is now clean with only essential files
    - All remaining files serve a purpose in the project

--- start-dev-server-5500.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Start the static file server on port 5500
-REM Make sure the main server (port 3000) is already running!
-cd /d "%~dp0"
-echo Starting Development Server on port 5500...
-echo Make sure the main server on port 3000 is running!
-npm run serve-5500
-pause
